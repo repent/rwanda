cb6a5de8494a7913587cd333a8bed8f717619349
Dan Hetherington <dan.hetherington@gmail.com>
2015-03-18 18:22:05 +0200

    Start working with full dataset, still not clean yet

--- lib/rwanda.rb
+++ lib/rwanda.rb
@@ -1,76 +1,48 @@
-require "rwanda/version"
-require "rwanda/data"
-require "fuzzy_match"
+#!/usr/bin/env ruby
+
+require 'rwanda/version'
+#require 'rwanda/data'
+require 'fuzzy_match'
+require 'csv'
+require 'pry'
+
+#binding.pry
+
+DATA = 'lib/rwanda/data.csv'
+
+class Village
+  attr_accessor :province, :district, :sector, :cell, :village
+  
+  #def initialize(province, district, sector, cell, village)
+  def initialize(row)
+    #@province,@district,@sector,@cell,@village=province,district,sector,cell,village
+    @province,@district,@sector,@cell,@village=row['province'],row['district'],row['sector'],row['cell'],row['village']
+  end
+  def to_s
+    "#{@province}/#{@district}/#{@sector}/#{@cell}/#{@village}"
+  end
+end
 
 class Rwanda
-  # Province, District, Sector, Cell?, Village?
+  attr_accessor :villages
   
+  def initialize
+    @villages = []
+    CSV.foreach(DATA, headers: :first_row) do |row|
+      villages << Village.new(row)
+    end
+  end
   # Singular Ofs ((
-  
-  def district_of(sector)
-    districts_with_sectors.each do |d,s_array|
-      return d if s_array.include? sector
-    end
-    nil
-  end
-  def province_of(district)
-    Province.each do |p,d_array|
-      return p if d_array.include? district
-    end
-    nil
-  end
-  
   # )) Plural Ofs ((
-  
-  def districts_of(province)
-    return nil unless Province[province]
-    Province[province].keys
-  end
-  def sectors_of(district)
-    districts_with_sectors[district]
-  end
-
   # )) Lists ((
+  def provinces; @villages.collect{|v| v.province}.uniq; end
+  def districts; @villages.collect{|v| v.district}.uniq; end
+  def sectors; @villages.collect{|v| v.sector}.uniq; end
   
-  def provinces; Province.keys; end
-  def districts; Province.collect {|p,d| d.collect {|d,s| d}}.flatten; end
-  def sectors; Province.collect {|p,d| d.collect {|d,s| s}}.flatten; end
-  
-  # )) Fuzzy Matching ((
-  
-  def district_like(district)
-    @fd ||= FuzzyMatch.new(districts)
-    @fd.find(district)
-  end
-  def province_like(province)
-    @fp = FuzzyMatch.new(provinces)
-    @fp.find(province)
-  end
-  def sector_like(sector)
-    @fs = FuzzyMatch.new(sectors)
-    @fs.find(sector)
-  end
-  
-  # )) Existence ((
-  
-  def province?(province); provinces.include? province; end
-  def district?(district); districts.include? district; end
-  def sector?(sector); sectors.include? sector; end
-  
-  # ))
-  
-  private
-  
-  def districts_with_sectors # flatten out province layer
-    Province.each_with_object({}) { |(p,d_hash),h| h.merge! d_hash }
-  end
-  #def sectors_with_cells
-  #  all_sectors_with_cells = {}
-  #  districts_with_sectors.each do |d,s_hash|
-  #    all_sectors_with_cells.merge!(s_hash)
-  #  end
-  #  all_sectors_with_cells
-  #end
-  #def sector_of(cell)
-  #end
+  # )) Matching
 end
+
+#rw=Rwanda.new
+#puts rw.villages
+
+#binding.pry
--- rwanda.gemspec
+++ rwanda.gemspec
@@ -21,5 +21,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec"
+  spec.add_development_dependency "pry"
   spec.add_runtime_dependency "fuzzy_match", "~> 2.1.0"
 end
