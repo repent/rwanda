79d97b8147d51f5f00fa0b2b180047b047d894c2
Dan Hetherington <dan.hetherington@gmail.com>
2015-03-23 18:29:33 +0200

    Working through the tests

--- lib/rwanda.rb
+++ lib/rwanda.rb
@@ -10,6 +10,15 @@ require 'pry'
 
 DATA = 'lib/rwanda/data.csv'
 
+class Array
+  def select_first
+    self.each do |el|
+      return el if yield(el)
+    end
+    nil
+  end
+end
+
 class Village
   attr_accessor :province, :district, :sector, :cell, :village
   
@@ -25,6 +34,13 @@ end
 
 class Rwanda
   attr_accessor :villages
+  RW = {
+    'Northern Province' => 'Amajyaruguru',
+    'Southern Province' => 'Amajyepfo',
+    'Eastern Province' => 'Iburasirazuba',
+    'Western Province' => 'Iburengerazuba',
+    'Kigali City' => 'Umujyi wa Kigali'
+  }
   
   def initialize
     @villages = []
@@ -33,16 +49,43 @@ class Rwanda
     end
   end
   # Singular Ofs ((
+  def province_of(district, rw=false)
+    village = @villages.select_first {|v| v.district == district}
+    if village
+      if rw then RW[village.province] else village.province end
+    else
+      nil
+    end
+  end
+  def district_of(sector)
+    village = @villages.select_first {|v| v.sector == sector}
+    village ? village.district : nil
+  end
   # )) Plural Ofs ((
+  def districts_of(province)
+    districts = @villages.select {|v| v.province == province }.collect {|v| v.district}.uniq
+    #binding.pry
+    districts.empty? ? nil : districts
+  end
+  def sectors_of(district)
+    sectors = @villages.select {|v| v.district == district }.collect {|v| v.sector}.uniq
+    sectors.empty? ? nil : sectors
+  end
   # )) Lists ((
   def provinces; @villages.collect{|v| v.province}.uniq; end
   def districts; @villages.collect{|v| v.district}.uniq; end
-  def sectors; @villages.collect{|v| v.sector}.uniq; end
+  # already introduces ambiguity from sectors down: 37 districts are duplicate names
+  def sectors
+    @sectors ||= @villages.collect {|v| [v.district, v.sector] }.uniq.collect {|ds| ds[1]}.sort
+    #@villages.collect{|v| v.sector}.uniq
+  end
   
   # )) Matching
+  def division_like(division)
+  
+  end
 end
 
 #rw=Rwanda.new
 #puts rw.villages
 
-#binding.pry
